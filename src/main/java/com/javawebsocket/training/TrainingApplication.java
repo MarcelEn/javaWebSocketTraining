5fb3dbefd96b5a9f7a9eea14cc9804529ec3c935
Marcel Engler <mengler@united-internet.de>
2018-04-27 07:52:03 +0200

    added Spring

---
+++ src/main/java/com/javawebsocket/training/TrainingApplication.java
@@ -0,0 +1,12 @@
+package com.javawebsocket.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingApplication.class, args);
+	}
+}
